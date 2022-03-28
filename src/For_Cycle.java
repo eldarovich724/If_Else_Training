8ff89ae42b6fc58c0bee75eb6ddf798bcec9eca7
evakhidov <evakhidov724@gmail.com>
2022-03-28 17:07:46 -0400

    Initial commit

---
+++ src/For_Cycle.java
@@ -0,0 +1,40 @@
+public class For_Cycle {
+
+    public static void main(String[] args) {
+
+//        int count = 10;
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < count; j++)
+//            System.out.print(j + " ");
+//            System.out.println();
+//            count--;
+//        }
+        System.out.println();
+//        int count2 = 0;
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < count2 + 1; j++)
+//                System.out.print(j + " ");
+//            System.out.println();
+//            count2 = count2 + 1;
+//        }
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < count; j++)
+//                System.out.print(j + " ");
+//            System.out.println();
+//            count = count - 1;
+//        }
+        System.out.println();
+        int count2 = 9;
+        for (int i = 1; i < 11; i++) {
+            for (int j = -1; j < count2; j++) {
+                System.out.print(count2 + " ");
+//                System.out.println();
+                count2--;
+            }
+        }
+    }
+}
+
+
+
+
---
+++ src/If_Else_statement.java
@@ -0,0 +1,46 @@
+public class If_Else_statement {
+
+//    public static void print(int a, String b) {
+//        int age = 0;
+//        String edu = "you finished school";
+//        if ((age >= 18) && (edu == "you finished school")) {
+//            System.out.println("you can live alone");
+//        } else System.out.println("you can not live alone");
+//    }
+
+    public static void main(String[] args) {
+
+//        int a = 55;
+//        int b = 88;
+//        int c = 508;
+//
+//        if (a > b && a > c) {
+//            System.out.println("biggest is a: " + a);
+//        } else System.out.println("b or c are bigger than a");
+//        if (a < b && b > c) {
+//            System.out.println(" biggest is b: " + b);
+//        } if (c > a && c > b) {
+//            System.out.println("biggest is c: " + c);
+//        }
+
+        int age = 5;
+        String edu = "you finished school";
+        if (age >= 120) {
+            System.out.println("you are too old");
+        } else {
+            if ((age >= 18) && (edu == "you finished school")) {
+                System.out.println("you can live alone");
+            } else {
+                System.out.println("you can not live alone");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
