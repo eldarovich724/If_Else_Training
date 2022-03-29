5f88db2a10f2199bfb91eebd0321d0d4cc828312
Eldar Vakhidov <evakhidov724@gmail.com>
2022-03-28 21:37:03 -0400

    hw 5.2 done.

--- src/For_Cycle.java
+++ src/For_Cycle.java
@@ -23,15 +23,45 @@ public class For_Cycle {
 //            System.out.println();
 //            count = count - 1;
 //        }
-        System.out.println();
-        int count2 = 9;
-        for (int i = 1; i < 11; i++) {
-            for (int j = -1; j < count2; j++) {
-                System.out.print(count2 + " ");
-//                System.out.println();
-                count2--;
+//        System.out.println();
+//        int count2 = 9;
+//        for (int i = 1; i < 11; i++) {
+//            for (int j = -1; j < count2; j++) {
+//                System.out.print(count2 + " ");
+////                System.out.println();
+//                count2--;
+//            }
+//        }
+        for (int i = 9; i >= 0; i--) {
+            for (int j = 0; j <= i ; j++) {
+                System.out.print(j + " ");
             }
+            System.out.println();
         }
+
+        for (int i = 9; i >= 0; i--) {
+            for (int j = i; j < 9 ; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i ; j++) {
+                System.out.print(j + " ");
+                }
+            System.out.println();
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            for (int j = i; j < 9 ; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 0; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
 
